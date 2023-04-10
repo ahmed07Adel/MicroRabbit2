b97448cc194adea239a851056bdf10001027ae5e
Ahmed Adel Mohamed <63484238+ahmed07Adel@users.noreply.github.com>
2023-04-10 17:14:37 +0200

    Implement interface for domain core

---
+++ MicroRabbit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,21 @@
+﻿using MicroRabbit.Domain.Core.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        //Sent To Bus
+        Task SendCommand<T> (T command) where T : Command;        
+
+        // publish any type of Event
+        void Publish<T> (T @event) where T : Events.Event;
+        void Subscribe<T, TH>()
+            where T : Events.Event
+            where TH : IEventHandler<T>;
+    }
+}
---
+++ MicroRabbit.Domain.Core/Commands/Command.cs
@@ -0,0 +1,19 @@
+﻿using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        //sending message across our bus
+        public DateTime TimeStamp { get; set; }
+        public Command()
+        {
+            TimeStamp = DateTime.Now;   
+        }
+    }
+}
